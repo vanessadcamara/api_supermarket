8575fa65bf3d4c915d047d805a14aab6e6fd817b
Vanessa de Souza Câmara <vanessadcamara@gmail.com>
2025-02-07 10:09:14 -0400

    Adicionar configuração e rota inicial do arquivo main

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-if '__name__' == '__main__':
-    print('Hello, World!')
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+import uvicorn
+
+app = FastAPI()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Rota raiz
+@app.get("/")
+def read_root():
+    return JSONResponse(content={"message": "Supermarket Backend API is running!"})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)

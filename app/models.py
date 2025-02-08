b1cb99258ce81c1c10d8dc809d6f5a07a3d4047c
Vanessa de Souza Câmara <vanessadcamara@gmail.com>
2025-02-07 21:25:44 -0400

    Refatoração do relacionamento das tabelas do banco de dados

--- app/models.py
+++ app/models.py
@@ -1,15 +1,18 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
+
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     cpf = Column(String, unique=True, nullable=False)
+    sales = relationship("Sales", back_populates="users")
 class Category(Base):
     __tablename__ = "category"
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String, nullable=False)
+    products = relationship("Product", back_populates="category")
 class Product(Base):
     __tablename__ = "product"
     id = Column(Integer, primary_key=True, index=True)
